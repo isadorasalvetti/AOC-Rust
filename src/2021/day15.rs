32abbc4f2b433d2d871c8199dea272fa9b1e7430
Isadora Albrecht Salvetti <Isadora.Albrecht@king.com>
2021-12-15 11:35:27 +0100

    day15

--- src/2021/day15.rs
+++ src/2021/day15.rs
@@ -1,22 +1,23 @@
-use std::{io::BufRead, collections::{HashMap, HashSet}, ops::Rem};
+use std::{io::BufRead, collections::{HashMap, HashSet, BinaryHeap}, ops::Rem};
 use std::cmp::Ordering;
 use aoc2019::utils::read_buffer;
 
 const starting_pos: (i32, i32) = (0, 0);
-const target_pos: (i32, i32) = (100*5-1, 100*5-1);
+const target_pos_p1: (i32, i32) = (100*1-1, 100*1-1);
+const target_pos_p2: (i32, i32) = (100*5-1, 100*5-1);
 
 #[derive(Copy, Clone, Eq, PartialEq)]
-struct State {
-    cost: usize,
-    position: usize,
+struct Cost {
+    cost: u32,
+    pos: (i32, i32),
 }
-impl Ord for State {
+impl Ord for Cost {
     fn cmp(&self, other: &Self) -> Ordering {
         other.cost.cmp(&self.cost)
-            .then_with(|| self.position.cmp(&other.position))
+            .then_with(|| self.pos.cmp(&other.pos))
     }
 }
-impl PartialOrd for State {
+impl PartialOrd for Cost {
     fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
         Some(self.cmp(other))
     }
@@ -38,42 +39,35 @@ fn main(){
     }
 
     let mut explored = HashSet::<(i32, i32)>::new();
-    let mut sums = HashMap::<(i32, i32), u32>::new();
-    let mut sorted_keys = Vec::<(i32, i32)>::new();
-    let mut next = starting_pos;
-    sums.insert(starting_pos, 0);
-    explored.insert((0, 0));
+    let mut sums = BinaryHeap::<Cost>::new();
+    let mut next ;
+    sums.push(Cost {pos: starting_pos, cost: 0} );
 
     'bob: loop {
+        next = sums.pop().unwrap();
+        if explored.contains(&next.pos){ continue; }
+
+        //let tmp: Vec<_> = sums.clone().into_iter().map(|x|x.pos).collect();
+        //println!("{:?}", tmp);
+
         let new_candidates = [
-            (next.0, next.1 + 1),
-            (next.0, next.1 - 1),
-            (next.0 + 1, next.1),
-            (next.0 - 1, next.1)
+            (next.pos.0, next.pos.1 + 1),
+            (next.pos.0, next.pos.1 - 1),
+            (next.pos.0 + 1, next.pos.1),
+            (next.pos.0 - 1, next.pos.1)
         ];
 
         for candidate in new_candidates {
             if !risk.contains_key(&candidate) || explored.contains(&candidate){ continue; }
-            if candidate == target_pos { 
-                sums.insert(candidate, sums[&next] + risk[&candidate]);
+            let curr_risk = next.cost + risk[&candidate];
+            if candidate == target_pos_p2 { 
+                println!("Part2: {:?}", curr_risk);
                 break 'bob; 
             }
-
-            let curr_risk = sums[&next] + risk[&candidate];
-            add_candidate(&mut sorted_keys, &mut sums, candidate, curr_risk);
+            sums.push(Cost {pos: candidate, cost: curr_risk});
         }
-        sums.remove(&next);
-
-        next = sums.keys().min_by(|a, b| sums[a].cmp(&sums[b])).unwrap().clone();
-        explored.insert(next);
+        explored.insert(next.pos);
     }
-
-    println!("Part2: {:?}", &sums[&target_pos]);
 }
 
-fn add_candidate(sorted: &mut Vec<(i32, i32)>, dict: &mut HashMap<(i32, i32), u32>, candidate: (i32, i32), val: u32){
-    if dict.contains_key(&candidate){
-        *dict.entry(candidate).or_default() = u32::min(dict[&candidate], val)
-    }
-    else { dict.insert(candidate, val); }
-}
+
